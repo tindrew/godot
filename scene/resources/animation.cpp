1b78d010f1a1765fcb133e2dc413757f5feec789
The Rizzler <rizz@skibidi.com>
2024-10-01 02:53:30 +0200

    Revert "Merge pull request #97380 from TheSofox/handle_modes_default_fix"
    This reverts commit 4c5e879cc57c47f6225a22a56a1acdc016c95470, reversing
    changes made to 65c94ec8731914fbadafc9ba2535dd83a00cca14.

--- scene/resources/animation.cpp
+++ scene/resources/animation.cpp
@@ -321,12 +321,8 @@ bool Animation::_set(const StringName &p_name, const Variant &p_value) {
 				Vector<real_t> times = d["times"];
 				Vector<real_t> values = d["points"];
 #ifdef TOOLS_ENABLED
-				Vector<int> handle_modes;
-				if (d.has("handle_modes")) {
-					handle_modes = d["handle_modes"];
-				} else {
-					handle_modes.resize_zeroed(times.size());
-				}
+				ERR_FAIL_COND_V(!d.has("handle_modes"), false);
+				Vector<int> handle_modes = d["handle_modes"];
 #endif // TOOLS_ENABLED
 
 				ERR_FAIL_COND_V(times.size() * 5 != values.size(), false);
