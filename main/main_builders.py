ded7437df5289214234838c47ef7beeca3597181
mallory <53388337+riko@users.noreply.github.com>
2024-10-04 19:33:02 -0400

    add newline

--- main/main_builders.py
+++ main/main_builders.py
@@ -32,8 +32,9 @@ def make_splash_editor(target, source, env):
         g.write("/* THIS FILE IS GENERATED DO NOT EDIT */\n")
         g.write("#ifndef BOOT_SPLASH_EDITOR_H\n")
         g.write("#define BOOT_SPLASH_EDITOR_H\n")
+        
         # The editor splash background color is taken from the default editor theme's background color.
-        # This helps achieve a visually "smoother" transition between the splash screen and the editor.
+        # This helps achieve a visually "smoother" transition between the splash screen and the editor.'
         g.write("static const Color boot_splash_editor_bg_color = Color(0.125, 0.145, 0.192);\n")
         g.write("static const unsigned char boot_splash_editor_png[] = {\n")
         for i in range(len(buf)):
